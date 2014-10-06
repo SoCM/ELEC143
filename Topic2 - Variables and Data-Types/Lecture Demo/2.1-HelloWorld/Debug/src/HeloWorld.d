8561c33bceb89ef06459961c910d710fb28632ec
Nicholas Outram <nicholas.outram@plymouth.ac.uk>
2014-10-06 08:31:07 +0100

    Topic 2

---
+++ Topic2 - Variables and Data-Types/Lecture Demo/2.1-HelloWorld/Debug/src/HeloWorld.d
@@ -0,0 +1,1 @@
+src/HeloWorld.d: ../src/HeloWorld.c
---
+++ Topic2 - Variables and Data-Types/Lecture Demo/2.1-HelloWorld/src/HeloWorld.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : HeloWorld.c
+ Author      : Nicholas Outram
+ Version     :
+ Copyright   : Plymouth University
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hello World"); // prints Hello World
+	return EXIT_SUCCESS;
+}
